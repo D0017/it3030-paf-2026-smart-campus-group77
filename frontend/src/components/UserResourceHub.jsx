869ce42512c264e4205794c68e8ce45c2b405bf4
DamindiHimasha <damindihimasha23@gmail.com>
2026-04-29 07:59:00 +0530

    Update UserResourceHub.jsx

--- frontend/src/components/UserResourceHub.jsx
+++ frontend/src/components/UserResourceHub.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; // navigate සඳහා මෙය අනිවාර්යයි
+import { useNavigate } from 'react-router-dom'; 
 import AssetService from '../services/AssetService';
 
 const COLORS = {
